2e3ad6164a3f69477469f8fcaa83b5206ab6f6f8
maya <770257551@qq.com>
2024-09-03 10:43:20 +0800

    feature: support user-stat service

---
+++ user-stat/src/config.rs
@@ -0,0 +1,38 @@
+use anyhow::{bail, Result};
+use std::fs::File;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AppConfig {
+    pub server: ServerConfig,
+    pub auth: AuthConfig,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ServerConfig {
+    pub port: u16,
+    pub db_url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AuthConfig {
+    pub pk: String,
+}
+
+impl AppConfig {
+    pub fn load() -> Result<Self> {
+        // read from ./app.yml, or /etc/config/app.yml, or from env CHAT_CONFIG
+        let ret = match (
+            File::open("user_stat.yaml"),
+            File::open("/etc/config/user_stat.yaml"),
+            std::env::var("USER_STATS_CONFIG"),
+        ) {
+            (Ok(file), _, _) => serde_yaml::from_reader(file),
+            (_, Ok(file), _) => serde_yaml::from_reader(file),
+            (_, _, Ok(path)) => serde_yaml::from_reader(File::open(path)?),
+            _ => bail!("Failed to load config file"), // _ => return Err(anyhow::anyhow!("Failed to load config file")),
+        };
+        Ok(ret?)
+    }
+}
